f7f73b0bef94a0d57f89599eeb64d31c00e50e0a
Ivan Moll <ivanmoll07@gmail.com>
2023-03-16 09:56:47 +0100

    Initial Commit

---
+++ PadelScore Watch App/PadelScoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  PadelScoreApp.swift
+//  PadelScore Watch App
+//
+//  Created by Ivan Moll on 16/3/23.
+//
+
+import SwiftUI
+
+@main
+struct PadelScore_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
