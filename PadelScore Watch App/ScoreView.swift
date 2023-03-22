464d08f377a7272c7650ba82fd4e1a0a3d0d4927
Ivan Moll <ivanmoll07@gmail.com>
2023-03-23 00:12:10 +0100

    Add binding variables

--- PadelScore Watch App/ScoreView.swift
+++ PadelScore Watch App/ScoreView.swift
@@ -9,19 +9,38 @@ import SwiftUI
 
 struct ScoreView: View {
     @EnvironmentObject var matchModel:MatchModel
+    @State var currentPointsTeamA: String = "0"
+    @State var currentPointsTeamB: String = "0"
+    @State var currentTieBreakPointsTeamA: String = "0"
+    @State var currentTieBreakPointsTeamB: String = "0"
     
     var body: some View {
         VStack {
-            TeamAView()
+            TeamAView(
+                currentPointsTeamA: $currentPointsTeamA,
+                currentPointsTeamB: $currentPointsTeamB,
+                currentTieBreakPointsTeamA: $currentTieBreakPointsTeamA,
+                currentTieBreakPointsTeamB: $currentTieBreakPointsTeamB
+            )
             Divider()
               .frame(height: 2)
               .padding(.horizontal, 10)
-            TeamBView()
+            TeamBView(
+                currentPointsTeamA: $currentPointsTeamA,
+                currentPointsTeamB: $currentPointsTeamB,
+                currentTieBreakPointsTeamA: $currentTieBreakPointsTeamA,
+                currentTieBreakPointsTeamB: $currentTieBreakPointsTeamB
+            )
         }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
+    @State var currentPointsTeamA: String = "0"
+    @State var currentPointsTeamB: String = "0"
+    @State var currentTieBreakPointsTeamA: String = "0"
+    @State var currentTieBreakPointsTeamB: String = "0"
+    
     static var previews: some View {
         ScoreView()
             .environmentObject(MatchModel())
